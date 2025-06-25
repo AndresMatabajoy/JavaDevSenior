e45e17a10904fa0544cf22705fed647e46c36875
Leonardo Matabajoy <leonardomatabajoygiraldo4@gmail.com>
2025-06-25 15:27:34 -0500

    First commit

---
+++ modulo1/clase 4/vscode/modulo1/clase4/vscode/modulo1/clase4/practica4.java
@@ -0,0 +1,9 @@
+package vscode.modulo1.clase4;
+public class practica4 {
+    public static void main(String[] args) {
+        var a = 10;
+        var b = 20;
+        var c = a + b;
+        System.out.println("La suma de " + a + " y " + b + " es: " + c);
+    }
+}
---
+++ modulo1/clase1/src/App.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class App {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Mi ventana Swing");
+        frame.setSize(400, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
